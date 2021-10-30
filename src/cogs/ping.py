b1f26bc229ad99fc066f1805bd2dbf0ae49976d0
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-10-30 12:31:10 -0400

    Finished up ping cog

--- src/cogs/ping.py
+++ src/cogs/ping.py
@@ -3,7 +3,6 @@ from discord.ext import commands, tasks
 import datetime
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option
-import asyncio
 
 async def cache_afk_messages(bot, main):
     """A function that caches our stuff"""
@@ -11,8 +10,7 @@ async def cache_afk_messages(bot, main):
     afk_dict = {}
     cursor = main.find()
     for document in await cursor.to_list(length=300):
-        afk_dict.update(document.get("_id"), document.get("message"))
-    
+        afk_dict[document.get("_id")] = document.get("message")
     bot.afk_dict = afk_dict
 
 class Ping(commands.Cog):
@@ -40,9 +38,34 @@ class Ping(commands.Cog):
         # ignore ourself since we don't wanna do recursion
         if msg.author == self.bot.user:
             return
-        #elif
+        for user_id in self.bot.afk_dict.keys().copy():
+            user_id = int(user_id)
 
+            member = msg.guild.get_member(user_id)
 
+            if member.mentioned_in(msg):
+                embed = discord.Embed(
+                    title=f"{member.display_name} is AFK",
+                    description=self.bot.afk_dict.get(user_id),
+                    timestamp=datetime.datetime.utcnow(),
+                    color=msg.author.color
+                )
+                await msg.channel.send(embed=embed)
+
+            if msg.author.id == user_id:
+                del self.bot.afk_dict[user_id]
+                await self.main.delete_one({"_id": msg.author.id})
+                embed = discord.Embed(
+                    title=f"Welcome Back!",
+                    description=f"""I have removed your afk for you {msg.author.mention}!""",
+                    timestamp=datetime.datetime.utcnow(),
+                    color=msg.author.color
+                )
+                return await msg.channel.send(embed=embed)
+
+            
+            
+            
     @cog_ext.cog_slash(
         name="afk_set",
         description="""Set an afk message""",
@@ -59,8 +82,7 @@ class Ping(commands.Cog):
     async def _afk_set(self, ctx: SlashContext, message):
         """Set an afk message"""
         document = await self.main.find_one({"_id": ctx.author.id})
-
-        print(document)
+        self.bot.afk_dict[ctx.author.id] = message
         if not document:
             new_document = {
                 "_id": ctx.author.id,
@@ -78,6 +100,7 @@ class Ping(commands.Cog):
             color=discord.Color.green()
         )
         await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Ping(bot))
